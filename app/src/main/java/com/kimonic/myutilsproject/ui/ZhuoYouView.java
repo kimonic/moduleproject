1831a26a6604b3fe628bf45409db7aea3ab84083
Administrator <2031382182@qq.com>
2018-01-29 16:45:16 +0800

    20180124  扇形与扇形叠加绘制可以显示两边是圆弧的矩形

--- app/src/main/java/com/kimonic/myutilsproject/ui/ZhuoYouView.java
+++ app/src/main/java/com/kimonic/myutilsproject/ui/ZhuoYouView.java
@@ -9,12 +9,16 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
+import android.graphics.Region;
 import android.support.annotation.Nullable;
 import android.support.constraint.solver.widgets.ConstraintWidget;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.kimonic.myutilsproject.R;
+import com.kimonic.utilsmodule.utils.ToastUtils;
 
 /**
  * * ===============================================================
@@ -56,17 +60,10 @@ public class ZhuoYouView extends View {
     }
 
     private void initView() {
-        bitmap= BitmapFactory.decodeResource(getResources(), R.drawable.zhuoyou001);
-        rectF=new RectF();
-        path=new Path();
-        /**
-         *515,735
-
-         */
-
-
-
-        shadePaint=new Paint();
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.zhuoyou001);
+        rectF = new RectF();
+        path = new Path();
+        shadePaint = new Paint();
         shadePaint.setColor(Color.parseColor("#007000"));
         shadePaint.setStyle(Paint.Style.FILL);
 
@@ -74,56 +71,97 @@ public class ZhuoYouView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
+        Log.e("TAG", "onDraw: -----绘制图形-------111");
 
-        int width=getWidth();
-        int height=getHeight();
-        rectF.top=0;
-        rectF.left=0;
-        rectF.right=515;
-        rectF.bottom=735;
+        int width = getWidth();
+        int height = getHeight();
+        rectF.top = 0;
+        rectF.left = 0;
+//
+//        rectF.right = 515;
+//        rectF.bottom = 735;
 
-        float scaleX=width/515;
-        float scaleY=height/735;
+//        width=500;
+//        height=1800;
+
+        rectF.right = width;
+        rectF.bottom = height;
+
+        float scaleX = width / 515f;
+        float scaleY = height / 735f;
+
+        Log.e("TAG", "onDraw: 绘制图形----width-"+width);
+        Log.e("TAG", "onDraw: 绘制图形----height-"+height);
+        Log.e("TAG", "onDraw: 绘制图形----scaleX-"+scaleX);
+        Log.e("TAG", "onDraw: 绘制图形----scaleY-"+scaleY);
 
 
-        path.moveTo(283,322);
-        path.lineTo(269,321);
-        path.lineTo( 204,404);
-        path.lineTo(281,404);
-        path.lineTo(311,423);
-        path.lineTo(405,425);
-        path.lineTo(433,405);
-        path.lineTo(511,405);
-        path.lineTo(510,396);
-        path.lineTo(450,321);
-        path.lineTo( 430,321);
-        path.lineTo(432,369);
-        path.lineTo(293,366 );
-        path.lineTo( 286,366);
-        path.lineTo(286,322);
+//        float scaleX = 1;
+//        float scaleY = 1;
+
+//        float scale = 1.5f * scaleX;
+//        float scale1 = 1.5f * scaleY;
+
+//        path.offset(0 * scaleX, 150 * scaleY);
+
+        path.moveTo(134 * scaleX, 459 * scaleY);
+        path.lineTo(156 * scaleX, 459 * scaleY);
+        path.lineTo(163 * scaleX, 520 * scaleY);
+        path.lineTo(356 * scaleX, 520 * scaleY);
+        path.lineTo(356 * scaleX, 460 * scaleY);
+        path.lineTo(381 * scaleX, 460 * scaleY);
+        path.lineTo(461 * scaleX, 558 * scaleY);
+        path.lineTo(461 * scaleX, 570 * scaleY);
+        path.lineTo(360 * scaleX, 570 * scaleY);
+        path.lineTo(319 * scaleX, 600 * scaleY);
+        path.lineTo(195 * scaleX, 600 * scaleY);
+        path.lineTo(155 * scaleX, 570 * scaleY);
+        path.lineTo(52 * scaleX, 570 * scaleY);
+        path.lineTo(52 * scaleX, 558 * scaleY);
         path.close();
+        Log.e("TAG", "onDraw: -----绘制图形-------222");
 
-        path.offset(-100,150);
+        canvas.drawBitmap(bitmap, null, rectF, null);
+        canvas.drawPath(path, shadePaint);
+        Log.e("TAG", "onDraw: -----绘制图形-------333");
+
+        /**
+         * 134,459
+         156,459
+         163,520
+         356,520
+         356,460
+         381,460
+         461,558
+         461,570
+         360,570
+         319,600
+         195,600
+         155,570
+         52,570
+         52,558
+         */
 
 
-//        path.moveTo(283*scaleX,322);
-//        path.lineTo(269*scaleX,321);
-//        path.lineTo( 204*scaleX,404);
-//        path.lineTo(281*scaleX,404);
-//        path.lineTo(311*scaleX,423);
-//        path.lineTo(405*scaleX,425);
-//        path.lineTo(433*scaleX,405);
-//        path.lineTo(511*scaleX,405);
-//        path.lineTo(510*scaleX,396);
-//        path.lineTo(450*scaleX,321);
-//        path.lineTo( 430*scaleX,321);
-//        path.lineTo(432*scaleX,369);
-//        path.lineTo(293*scaleX,366 );
-//        path.lineTo( 286*scaleX,366);
-//        path.lineTo(286*scaleX,322);
-//        path.close();
+    }
 
-        canvas.drawBitmap(bitmap,null,rectF,null);
-        canvas.drawPath(path,shadePaint);
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction()==MotionEvent.ACTION_DOWN){
+            RectF r=new RectF();
+            path.computeBounds(r, true);
+            Region region=new Region();
+            region.setPath(path, new Region((int)r.left,(int)r.top,(int)r.right,(int)r.bottom));
+            if (region.contains((int)event.getX(), (int)event.getY())){
+                ToastUtils.showToast(getContext(), "包含该点");
+            }else {
+                ToastUtils.showToast(getContext(), "不包含该点");
+
+            }
+            Log.e("","--判断点是否则范围内----"+region.contains((int)event.getX(), (int)event.getY()));
+        }
+
+
+        return super.onTouchEvent(event);
     }
 }
